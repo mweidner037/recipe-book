432b306f14a8c1a2fbdd4feae4bed65462533bad
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2021-09-25 12:53:00 -0400

    editable ingredients, recipe name

--- single_user/src/script.ts
+++ single_user/src/script.ts
@@ -1,36 +1,85 @@
+class Ingredient {
+  private _text: string;
+
+  public constructor(text: string) {
+    this._text = text;
+  }
+
+  /**
+   * @return An <li> element displaying this ingredient.
+   */
+  public toHTML(): HTMLLIElement {
+    const li = document.createElement("li");
+    li.setAttribute("class", "list-group-item");
+    // Add input/display elements to li.
+    const textbox = document.createElement("input");
+    textbox.className = "text-input";
+    textbox.type = "text";
+    textbox.value = this._text;
+    textbox.oninput = () => {
+      // Update the backing state when the text changes.
+      this._text = textbox.value;
+    };
+    li.appendChild(textbox);
+    return li;
+  }
+}
+
 class Recipe {
   private _title: string;
-  private _ingredients: string;
+  private _ingredients: Ingredient[];
 
   public constructor(title: string, ingredients: string) {
     this._title = title;
-    this._ingredients = ingredients;
+    this._ingredients = ingredients
+      .split(",")
+      .map((text) => new Ingredient(text));
   }
 
   // Converts the current instance to a li html element with some css classes
   // Similar to toString(), but for html
-  public toHTML(): HTMLElement {
-    let li: HTMLElement = document.createElement("li");
-    li.innerHTML = this._title;
-    li.onclick = () => this.updateIngredientsList();
+  public toHTML(): HTMLLIElement {
+    const li = document.createElement("li");
     li.setAttribute("class", "list-group-item");
+    li.style.position = "relative";
+    // Add input/display elements to li.
+    const textbox = document.createElement("input");
+    textbox.className = "text-input";
+    textbox.type = "text";
+    textbox.value = this._title;
+    textbox.oninput = () => {
+      // Update the backing state when the text changes.
+      this._title = textbox.value;
+    };
+    li.appendChild(textbox);
+    // Only allow editing after double click.
+    // We accomplish this by overlaying a div unless it is
+    // being edited.
+    const overlayDiv = document.createElement("div");
+    overlayDiv.className = "overlay-div";
+    overlayDiv.ondblclick = () => {
+      overlayDiv.hidden = true;
+      textbox.select(); // Select all
+    };
+    textbox.onblur = () => {
+      overlayDiv.hidden = false;
+    };
+    li.appendChild(overlayDiv);
+    // Clicking the li displays the recipe.
+    li.onclick = () => {
+      // Don't start editing text until double click.
+      // TODO
+      this.updateIngredientsList();
+    };
     return li;
   }
 
   // When a recipe is clicked, update the ingredients table with each ingredient
   private updateIngredientsList(): void {
     let ingredientList = document.getElementById("ingredient-list")!;
-    let ingredients = this._ingredients.split(",");
-
     ingredientList.innerHTML = "";
-    for (let ingredient of ingredients) {
-      if (ingredient === "") {
-        continue;
-      }
-      let li = document.createElement("li");
-      li.innerHTML = ingredient;
-      li.setAttribute("class", "list-group-item");
-      ingredientList.appendChild(li);
+    for (let ingredient of this._ingredients) {
+      ingredientList.appendChild(ingredient.toHTML());
     }
   }
 }
